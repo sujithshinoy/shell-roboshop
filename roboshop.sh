b3b127e28691b3f34a0ef5c9155efec396139e8d
sujithkumar <sujithshinoy@gmail.com>
2026-02-06 21:21:43 +0530

    actions

--- roboshop.sh
+++ roboshop.sh
@@ -5,13 +5,33 @@ AMI_ID="ami-0220d79f3f480ecf5"
 
 for instance in $@
 do
- aws ec2 run-instances 
+ INSTANCE_ID = $(aws ec2 run-instances \ 
  --image-id $AMI_ID \
  --instance-type t3.micro \ 
  --security-group-ids $SG_ID \ 
  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \ 
  --query 'Instances[0].InstanceId' \
- --output text  
+ --output text )
 
+  if [ $instance == "frontend" ]; then
+        IP=$(
+            aws ec2 describe-instances \
+            --instance-ids $INSTANCE_ID \
+            --query 'Reservations[].Instances[].PublicIpAddress' \
+            --output text
+        )
+        RECORD_NAME="$DOMAIN_NAME" # daws88s.online
+    else
+        IP=$(
+            aws ec2 describe-instances \
+            --instance-ids $INSTANCE_ID \
+            --query 'Reservations[].Instances[].PrivateIpAddress' \
+            --output text
+        )
+        RECORD_NAME="$instance.$DOMAIN_NAME" # mongodb.daws88s.online
+    fi    
+
+   echo "IP address $IP"
+   
 done
 
