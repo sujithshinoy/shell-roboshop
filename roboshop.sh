45b976222872c47e36ce8401c4b568162b4ea831
sujithkumar <sujithshinoy@gmail.com>
2026-02-06 22:27:55 +0530

    actions

--- roboshop.sh
+++ roboshop.sh
@@ -4,6 +4,7 @@ SG_ID="sg-0310cfa82e8e5f6c3" # replace with you ID
 AMI_ID="ami-0220d79f3f480ecf5"
 ZONE_ID="Z012461222YNWWRFVF52N"
 DOMINA_NAME="sujithshinoy.online"
+
 for instance in $@
 do
  INSTANCE_ID =$(aws ec2 run-instances \ 
@@ -36,7 +37,7 @@ do
    echo "IP address :$IP"
 
    aws route53 change-resource-record-sets \
-    --hosted-zone-id HOSTED-ZONE-ID
+    --hosted-zone-id $ZONE_ID \
     --change-batch '
      {
         "Comment": "Updating record",
